56167f819dc8bcb6529e5e66c2c211b1bcd771de
github nanakoshi <nanakoshis@gmail.com>
2025-05-10 09:46:01 +0900

    flutter_dotenvを使用して複数の環境設定を出しわける launch.jsonにargを追加

--- practice_app/lib/main.dart
+++ practice_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practice_app/components/colors.dart';
 import 'package:practice_app/view/api_screen.dart';
@@ -12,7 +13,27 @@ import 'view/my_home_pege.dart';
 import 'view/test_screen.dart';
 import 'view/url_launcher_screen.dart';
 
-void main() {
+void main() async {
+  /*
+  ビルドコマンドによって環境変数の出しわけを行う
+  例：flutter run --dart-define=FLAVOR=developmentでビルドした場合、デベロップモードで実行する
+  ターミナルで毎回実行には手間がかかるのでflutter run以降のコマンドは自動設定が可能
+  例えばVS Codeの場合はlaunch.jsonのargsで設定する
+  */
+
+  // dart-define から FLAVOR を取得
+  const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'development');
+
+    // 環境に応じた .env ファイルを読み込む
+  String envFile = '.env.development';
+  if (flavor == 'staging') {
+    envFile = '.env.staging';
+  } else if (flavor == 'production') {
+    envFile = '.env.production';
+  }
+
+  await dotenv.load(fileName: envFile);
+  
   runApp(
     // Riverpodを使用するためにProviderScopeを追加
     const ProviderScope(
@@ -26,6 +47,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: 環境設定の出しわけ確認
+    print("APP_MODE_NAME: ${dotenv.env['APP_MODE_NAME']}");
+    print("APP_ENV: ${dotenv.env['APP_ENV']}");
     // MaterialApp Googleの標準デザイン（マテリアルデザイン）の雛形を設定（テーマカラーなど）
     return MaterialApp(
       title: 'Flutter Demo',
